2b449ec45cc6592c7bef4eeec95e23f40d2a930b
Frank Zhao <frank@frankzhao.net>
2018-07-21 12:44:37 +1200

    Add sonar template and script

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -14,12 +14,18 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 # oc new-app -f ../templates/sonarqube.yaml --param .....
 
 # To be Implemented by Student
-oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar\
-  --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar\
-  --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db
-oc new-app https://github.com/wkulhanek/docker-openshift-sonarqube.git
-oc set env dc/docker-openshift-sonarqube SONARQUBE_JDBC_USERNAME=sonar,SONARQUBE_JDBC_PASSWORD=sonar,SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar
-oc expose svc/docker-openshift-sonarqube
-oc set resources dc/docker-openshift-sonarqube --requests=memory=1.5Gi,cpu=1 --limits=memory=3Gi,cpu=2
+oc new-app -f ../templates/sonarqube.template.yaml\
+  --param POSTGRESQL_USERNAME=sonar\
+  --param POSTGRESQL_PASSWORD=sonar\
+  --param POSTGRESQL_DATABASE=sonar\
+  --param POSTGRESQL_VOLUME=4Gi\
+  --param GUID=$GUID
+# oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar\
+#   --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar\
+#   --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db
+# oc new-app https://github.com/wkulhanek/docker-openshift-sonarqube.git
+# oc set env dc/docker-openshift-sonarqube SONARQUBE_JDBC_USERNAME=sonar,SONARQUBE_JDBC_PASSWORD=sonar,SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar
+# oc expose svc/docker-openshift-sonarqube
+# oc set resources dc/docker-openshift-sonarqube --requests=memory=1.5Gi,cpu=1 --limits=memory=3Gi,cpu=2
 
 
